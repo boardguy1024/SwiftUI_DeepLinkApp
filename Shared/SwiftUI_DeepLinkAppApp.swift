b12e8abd6c2d18ba0a21e5da0c338779213a90c0
boardguy1024 <boardguy1024@gmail.com>
2022-06-25 21:43:27 +0900

    Initial Commit

---
+++ Shared/SwiftUI_DeepLinkAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_DeepLinkAppApp.swift
+//  Shared
+//
+//  Created by park kyung seok on 2022/06/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_DeepLinkAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
